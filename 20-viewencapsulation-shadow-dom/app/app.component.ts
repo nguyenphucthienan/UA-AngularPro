c7ba5ea829b553c491471aa1ce58db705550f011
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-09 17:19:41 +0700

    ViewEncapsulation and Shadow DOM

---
+++ 20-viewencapsulation-shadow-dom/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      <example-one></example-one>
+      <example-two></example-two>
+      <example-three></example-three>
+    </div>
+  `
+})
+export class AppComponent { }
