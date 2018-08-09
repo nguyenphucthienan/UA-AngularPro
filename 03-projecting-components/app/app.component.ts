703723529f1f31e309f08de3fe7f9f655b281159
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-09 16:30:27 +0700

    Projecting and binding to components

---
+++ 03-projecting-components/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+import { User } from './auth-form/auth-form.interface';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      <auth-form (submitted)="createUser($event)">
+        <h3>Join us</h3>
+        <button type="submit">Submit</button>
+      </auth-form>
+      <auth-form (submitted)="loginUser($event)">
+        <h3>Login</h3>
+        <auth-remember 
+          (checked)="rememberUser($event)"></auth-remember>
+        <button type="submit">Login</button>
+      </auth-form>
+    </div>
+  `
+})
+export class AppComponent {
+  rememberMe: boolean = false;
+
+  rememberUser(remember: boolean) {
+    this.rememberMe = remember;
+  }
+
+  createUser(user: User) {
+    console.log('Create account', user);
+  }
+
+  loginUser(user: User) {
+    console.log('Login', user, this.rememberMe);
+  }
+}
