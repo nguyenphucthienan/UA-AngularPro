fcfdda001c723a51977187484e7e9622ae848643
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-17 13:55:15 +0700

    Custom form control base

---
+++ 40-custom-form-control-base/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    StockInventoryModule
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
