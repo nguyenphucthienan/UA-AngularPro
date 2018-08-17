e609d590071c9e18db2d78ebb75b92c6b0e559be
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-17 13:36:22 +0700

    Updating and resetting FormGroups and FormControls

---
+++ 39-reset-update-form-controls/app/stock-inventory/stock-inventory.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { StockInventoryComponent } from './containers/stock-inventory.component';
+import { StockBrandComponent } from './components/stock-brand/stock-brand.component';
+import { StockProductComponent } from './components/stock-product/stock-product.component';
+import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
+
+import { StockInventoryService } from './services/stock-inventory.service';
+
+@NgModule({
+  declarations: [
+    StockInventoryComponent,
+    StockBrandComponent,
+    StockProductComponent,
+    StockSelectorComponent
+  ],
+  providers: [
+    StockInventoryService
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  exports: [
+    StockInventoryComponent
+  ]
+})
+export class StockInventoryModule { }
