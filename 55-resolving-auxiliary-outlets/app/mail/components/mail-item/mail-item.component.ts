ae348117c287abd09a7f18847462b91b9efe6730
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-18 21:26:26 +0700

    Resolving data for auxiliary outlets

---
+++ 55-resolving-auxiliary-outlets/app/mail/components/mail-item/mail-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+import { Mail } from '../../models/mail.interface';
+
+@Component({
+  selector: 'mail-item',
+  styleUrls: ['mail-item.component.scss'],
+  template: `
+    <a
+      [routerLink]="['', { outlets: { pane: ['message', message.id] } }]"
+      routerLinkActive="active"
+      class="mail-item">
+      <h3>
+        {{ message.from }}
+        <span>{{ message.timestamp | date:'shortTime' }}</span>
+      </h3>
+      <p>{{ message.summary }}</p>
+    </a>
+  `
+})
+export class MailItemComponent {
+  @Input() message: Mail;
+}
